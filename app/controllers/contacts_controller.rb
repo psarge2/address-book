2ff550723cfede06b7b2ebf3ce4ea52b7bf9690d
psarge2 <paul@psarge.dev>
2022-05-09 14:20:37 -0400

    update

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -65,6 +65,6 @@ class ContactsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contact_params
-      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :home_address, :birthday, :general_note)
+      params.require(:contact).permit(:first_name, :last_name, :email, :phone, :home_address, :birthday, :general_note, :user_id)
     end
 end
