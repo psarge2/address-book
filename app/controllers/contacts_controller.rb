30768a728edc50e22a9a0703f1e65a562483b508
psarge2 <paul@psarge.dev>
2022-05-10 03:04:42 -0400

    edit style for html.erb forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   before_action :set_contact, only: %i[ show edit update destroy ]
-
+  before_action :authenticate_user!, except: [:index, :show]
   # GET /contacts or /contacts.json
   def index
     @contacts = Contact.all
